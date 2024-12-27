ce971c4c88d1d05e1b4d6051103bb30543d8cf71
Jorge Valencia <jorge.valencia.61752@gmail.com>
2024-12-27 10:38:57 -0600

    Se crea el proyecto java con SpringBoot

---
+++ src/main/java/com/javh/rest/foro/api_rest_foro/ApiRestForoApplication.java
@@ -0,0 +1,13 @@
+package com.javh.rest.foro.api_rest_foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javh/rest/foro/api_rest_foro/ApiRestForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javh.rest.foro.api_rest_foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
